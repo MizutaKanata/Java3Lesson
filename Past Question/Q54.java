75623be33db52540ea1fbd72cab084f549dc13b5
mizu <86910433+MizutaKanata@users.noreply.github.com>
2022-06-08 10:00:30 +0900

    Create Q54.java

---
+++ Past Question/Q54.java
@@ -0,0 +1,29 @@
+package Q54;
+
+public class Mondai {
+	interface Material{
+		void test();
+	}
+/*
+ * class Metal implements Material{
+ 
+	int temp;
+	Metal() {temp = 23;}
+	public void heat() {System.out.println("Extend");}
+}
+class BarnMetal extends Metal{
+	public void heat();
+	System.out.print("Barn");
+}
+public class Q2{
+	public void main(String[] args) {
+		Material m1 = new Metal();
+		Material m2 = new BarnMetal();
+		
+		m1.heat();
+		m2.heat
+		
+	}
+}*/
+}
+
