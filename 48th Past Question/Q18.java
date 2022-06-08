b3c87565e7a18c0bec9a525f6b4aa24aa34e860f
mizu <86910433+MizutaKanata@users.noreply.github.com>
2022-06-08 10:49:10 +0900

    Update and rename Q1.md to Q18.java

---
+++ 48th Past Question/Q18.java
@@ -0,0 +1,14 @@
+class A{
+	public String toString() {
+		return "こんにちは";
+	}
+}
+
+public class Q18 {
+	public static void main(String[] args) {
+		A a = new A();
+		//インスタンスaのtoストリング()メソッドが実行される
+		System.out.println(a);
+	}
+
+}
