c5d5266aa9fc96a1afa0332303ae30335875fb63
mizu <86910433+MizutaKanata@users.noreply.github.com>
2022-06-29 11:22:32 +0900

    Create Q25.java

---
+++ 51thPast Question/Q25.java
@@ -0,0 +1,14 @@
+
+public class Q25 {
+
+	public static void main(String[] args) {
+		boolean bool =false;
+		if(bool = true) {
+			System.out.println("真");
+		}else {
+			System.out.println("偽");
+		}
+
+	}
+
+}
